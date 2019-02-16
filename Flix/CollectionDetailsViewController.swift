1a3e0925c73980d1105061db5480e9ef78efe67e
Harry Zhang <harryzhang100@gmail.com>
2019-02-15 17:41:53 -0800

    adding a new superhero detail view controller file

---
+++ Flix/CollectionDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionDetailsViewController.swift
+//  Flix
+//
+//  Created by Harry Zhang on 2/15/19.
+//  Copyright © 2019 Harry Zhang. All rights reserved.
+//
+
+import UIKit
+
+class CollectionDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
